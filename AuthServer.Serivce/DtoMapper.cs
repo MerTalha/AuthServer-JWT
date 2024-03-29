93e8f2e32e8f974e73fe8f56a1be7cf2337724d2
Mert Şimşek <merttalhasimsek@gmail.com>
2024-03-29 09:34:26 +0300

    Mapper Update

--- AuthServer.Serivce/DtoMapper.cs
+++ AuthServer.Serivce/DtoMapper.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AuthServer.Core.DTOs;
+using AuthServer.Core.Models;
+using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,12 @@ using System.Threading.Tasks;
 
 namespace AuthServer.Service
 {
-    public class DtoMapper
+    class DtoMapper:Profile
     {
-
+        public DtoMapper()
+        {
+            CreateMap<ProductDto, Product>().ReverseMap();
+            CreateMap<UserAppDto, UserApp>().ReverseMap();
+        }
     }
 }
