a020f23aad734e7ccc1d97f6df0c72ee1dd84515
Mert Şimşek <merttalhasimsek@gmail.com>
2024-04-04 09:44:53 +0300

    CustomExceptionHandler Extension Update

--- AuthServer/Program.cs
+++ AuthServer/Program.cs
@@ -88,7 +88,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+else
+{
+    app.UseCustomException();
+}
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
